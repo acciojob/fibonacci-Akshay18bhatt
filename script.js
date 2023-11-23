8e4b88ff4fcfdc932ebca312b4636599728c847c
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-23 18:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function fibonacci(num) {
-// your code here
-	if(num==1 || num==0){
-		return 0;
-	}
-	if(num==2){
-		return 1;
-	}
+		let v=[0,1];
 
-	return (fibonacci(num-1)+fibonacci(num-2));
+	if(n==0)
+		return 0;
+	if(n==1)
+		return 0;
+
+	for(let i=2;i<=n;i++)
+		{
+			let sum=v[i-1]+v[i-2];
+			v.push(sum);
+		}
+
+	return v[n-1];
+}
+
+// your code here
+	// if(num==1 || num==0){
+	// 	return 0;
+	// }
+	// if(num==2){
+	// 	return 1;
+	// }
+
+	// return (fibonacci(num-1)+fibonacci(num-2));
 	// let num1= fibonacci(num-1);
 	// let num2= fibonacci(num-2);
 	// let result= num1+num2;
@@ -15,4 +31,4 @@ function fibonacci(num) {
 	// return result;
 }
 // console.log(fibonacci(0));
-// module.exports = fibonacci;
+module.exports = fibonacci;
